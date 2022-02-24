9b2a23ebe3e3665eb63206826d228bdf42810db3
Ilya Pukhov <IlyaPukhov@github.com>
2022-02-25 00:09:37 +0300

    Initial commit

--- src/com/ilyap/tasks/Main.java
+++ src/com/ilyap/tasks/Main.java
@@ -1,10 +1,17 @@
 package com.ilyap.tasks;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
         long start = System.currentTimeMillis();
-        System.out.println(Fibonacci.fib(43));
+        System.out.println(Fibonacci.fib(n));
         long end = System.currentTimeMillis();
+
         System.out.println(end - start);
+        scanner.close();
     }
 }
