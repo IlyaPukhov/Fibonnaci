db0a081d2deb92f3ec18970d9de8dae0893e286e
Ilya Pukhov <IlyaPukhov@github.com>
2022-03-05 16:09:41 +0300

    Modified data structure and dimensions

--- src/com/ilyap/tasks/Main.java
+++ src/com/ilyap/tasks/Main.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long n = scanner.nextLong();
+        long n;
+        try (Scanner scanner = new Scanner(System.in)) {
+            n = scanner.nextLong();
+        }
 
         long start = System.currentTimeMillis();
-        System.out.println("fib(" + n + "): " + Fibonacci.fib(n));
+        System.out.println(("fib(" + n + "): " + Fibonacci.fib(n)));
         long end = System.currentTimeMillis();
 
         System.out.println("Time spent: " + (end - start) + " ms.");
-        scanner.close();
     }
 }
